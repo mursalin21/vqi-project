e12149f84dc58eb94eea7a43c827503a9c8659e2
mahraj <maharajmursalin@gmail.com>
2022-06-12 16:45:40 +0600

    export update 12-6

--- backend/server.js
+++ backend/server.js
@@ -20,21 +20,21 @@ app.use("/api/qaTaskTracker", qaTrackerRoutes);
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
+	app.get("/", (req, res) => {
+		res.send("API is running...");
+	});
 }
 // ---------------- deployment ----------------
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}!`));
--- frontend/src/Screens/TaskList/TaskList.js
+++ frontend/src/Screens/TaskList/TaskList.js
@@ -1,181 +1,181 @@
 import React, { useEffect, useState } from "react";
 import {
-  Alert,
-  Badge,
-  Button,
-  Card,
-  Container,
-  ListGroup,
+	Alert,
+	Badge,
+	Button,
+	Card,
+	Container,
+	ListGroup,
 } from "react-bootstrap";
-import MainScreen from "../../Components/MainScreen";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  listAdminTasks,
-  updateAdminTaskAction,
-  adminExportList,
+	adminExportList,
+	listAdminTasks,
+	updateAdminTaskAction,
 } from "../../actions/qaTaskActions";
-import Loading from "../../Components/Loading";
 import ErrorMessage from "../../Components/ErrorMessage";
+import Loading from "../../Components/Loading";
+import MainScreen from "../../Components/MainScreen";
 
 const TaskList = () => {
-  const dispatch = useDispatch();
-  const adminTaskList = useSelector((state) => state.adminTaskList);
-  const adminTaskUpdate = useSelector((state) => state.adminTaskUpdate);
-  // const adminExportList = useSelector((state) => state.adminExportList);
+	const dispatch = useDispatch();
+	const adminTaskList = useSelector((state) => state.adminTaskList);
+	const adminTaskUpdate = useSelector((state) => state.adminTaskUpdate);
+	// const adminExportList = useSelector((state) => state.adminExportList);
 
-  const { loading, qaTasks, error } = adminTaskList;
-  const { taskUpdateLoading, taskUpdateError } = adminTaskUpdate;
+	const { loading, qaTasks, error } = adminTaskList;
+	const { taskUpdateLoading, taskUpdateError } = adminTaskUpdate;
 
-  const [isUpdating, setIsUpdating] = useState(false);
-  const [taskId, setTaskId] = useState("");
-  const [jobNumber, setJobNumber] = useState();
-  const [email, setEmail] = useState("");
-  const [taskReceivedDate, setTaskReceivedDate] = useState(new Date());
-  const [totalVolume, setTotalVolume] = useState();
-  const [editedVolume, setEditedVolume] = useState();
-  const [status, setStatus] = useState("Pending");
-  const [visible, setVisible] = useState(false);
+	const [isUpdating, setIsUpdating] = useState(false);
+	const [taskId, setTaskId] = useState("");
+	const [jobNumber, setJobNumber] = useState();
+	const [email, setEmail] = useState("");
+	const [taskReceivedDate, setTaskReceivedDate] = useState(new Date());
+	const [totalVolume, setTotalVolume] = useState();
+	const [editedVolume, setEditedVolume] = useState();
+	const [status, setStatus] = useState("Pending");
+	const [visible, setVisible] = useState(false);
 
-  useEffect(() => {
-    if (isUpdating === true) {
-      console.log(totalVolume);
-      dispatch(
-        updateAdminTaskAction(
-          taskId,
-          jobNumber,
-          email,
-          totalVolume,
-          editedVolume,
-          taskReceivedDate,
-          status
-        )
-      );
-      if (!status) return;
+	useEffect(() => {
+		if (isUpdating === true) {
+			console.log(totalVolume);
+			dispatch(
+				updateAdminTaskAction(
+					taskId,
+					jobNumber,
+					email,
+					totalVolume,
+					editedVolume,
+					taskReceivedDate,
+					status
+				)
+			);
+			if (!status) return;
 
-      setIsUpdating(false);
-    }
-    dispatch(listAdminTasks());
-  }, [
-    dispatch,
-    editedVolume,
-    isUpdating,
-    jobNumber,
-    email,
-    status,
-    taskId,
-    taskReceivedDate,
-    totalVolume,
-  ]);
+			setIsUpdating(false);
+		}
+		dispatch(listAdminTasks());
+	}, [
+		dispatch,
+		editedVolume,
+		isUpdating,
+		jobNumber,
+		email,
+		status,
+		taskId,
+		taskReceivedDate,
+		totalVolume,
+	]);
 
-  const handleSubmitAlert = () => {
-    setVisible(true);
+	const handleSubmitAlert = () => {
+		setVisible(true);
 
-    window.setTimeout(() => {
-      setVisible(false);
-    }, 2000);
-  };
+		window.setTimeout(() => {
+			setVisible(false);
+		}, 2000);
+	};
 
-  const handleExportCsv = () => {
-    dispatch(adminExportList());
-  };
+	const handleExportCsv = () => {
+		dispatch(adminExportList());
+	};
 
-  return (
-    <MainScreen title="Report List">
-      <Container
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <Button onClick={() => handleExportCsv()}>Export</Button>
-      </Container>
-      <Alert color="info" show={visible}>
-        Success! Task Updated!
-      </Alert>
-      <Container
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
-      >
-        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-        {loading && <Loading />}
-        {taskUpdateError && (
-          <ErrorMessage variant="danger">{error}</ErrorMessage>
-        )}
-        {taskUpdateLoading ? (
-          <Loading />
-        ) : (
-          qaTasks?.reverse().map((info, rank) => (
-            <Card key={rank} style={{ width: "20rem", margin: "10px" }}>
-              <Card.Header className="d-flex justify-content-between">
-                <p>{rank + 1}</p>
-                {info.status === "Pending" ? (
-                  <Button
-                    onClick={() => {
-                      setIsUpdating(true);
-                      setTaskId(info._id);
-                      setJobNumber(info.job_number);
-                      setEmail(info.email);
-                      setTaskReceivedDate(info.task_received_date);
-                      setTotalVolume(info.total_volume);
-                      setEditedVolume(info.edited_volume);
-                      setStatus("Done");
-                      console.log(info);
-                      handleSubmitAlert();
-                    }}
-                  >
-                    Complete
-                  </Button>
-                ) : (
-                  <h4>
-                    <Badge bg="success">Task Done</Badge>
-                  </h4>
-                )}
-              </Card.Header>
-              <ListGroup variant="flush">
-                <ListGroup.Item>Job Number: {info.job_number}</ListGroup.Item>
-                <ListGroup.Item>Email: {info.email}</ListGroup.Item>
-                <ListGroup.Item>
-                  Total Volume: {info.total_volume}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Edited Volume: {info.edited_volume}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Task Received Date: {info.task_received_date}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Task Created: {info.createdAt.substring(0, 10)}
-                  {" Time: "}
-                  {info.createdAt.substring(11, 19)}
-                </ListGroup.Item>
-                {info.status === "Done" && (
-                  <ListGroup.Item>
-                    Task Updated QA DONE: {info.createdAt.substring(0, 10)}
-                    {" Time: "}
-                    {info.createdAt.substring(11, 19)}
-                  </ListGroup.Item>
-                )}
-                {info.status === "Pending" ? (
-                  <ListGroup.Item>
-                    Status: <span style={{ color: "red" }}>{info.status}</span>
-                  </ListGroup.Item>
-                ) : (
-                  <ListGroup.Item>
-                    Status:{" "}
-                    <span style={{ color: "green" }}>{info.status}</span>
-                  </ListGroup.Item>
-                )}
-              </ListGroup>
-            </Card>
-          ))
-        )}
-      </Container>
-    </MainScreen>
-  );
+	return (
+		<MainScreen title="Report List Testing">
+			<Container
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					justifyContent: "space-evenly",
+				}}
+			>
+				<Button onClick={() => handleExportCsv()}>Export</Button>
+			</Container>
+			<Alert color="info" show={visible}>
+				Success! Task Updated!
+			</Alert>
+			<Container
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					justifyContent: "space-evenly",
+				}}
+			>
+				{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+				{loading && <Loading />}
+				{taskUpdateError && (
+					<ErrorMessage variant="danger">{error}</ErrorMessage>
+				)}
+				{taskUpdateLoading ? (
+					<Loading />
+				) : (
+					qaTasks?.reverse().map((info, rank) => (
+						<Card key={rank} style={{ width: "20rem", margin: "10px" }}>
+							<Card.Header className="d-flex justify-content-between">
+								<p>{rank + 1}</p>
+								{info.status === "Pending" ? (
+									<Button
+										onClick={() => {
+											setIsUpdating(true);
+											setTaskId(info._id);
+											setJobNumber(info.job_number);
+											setEmail(info.email);
+											setTaskReceivedDate(info.task_received_date);
+											setTotalVolume(info.total_volume);
+											setEditedVolume(info.edited_volume);
+											setStatus("Done");
+											console.log(info);
+											handleSubmitAlert();
+										}}
+									>
+										Complete
+									</Button>
+								) : (
+									<h4>
+										<Badge bg="success">Task Done</Badge>
+									</h4>
+								)}
+							</Card.Header>
+							<ListGroup variant="flush">
+								<ListGroup.Item>Job Number: {info.job_number}</ListGroup.Item>
+								<ListGroup.Item>Email: {info.email}</ListGroup.Item>
+								<ListGroup.Item>
+									Total Volume: {info.total_volume}
+								</ListGroup.Item>
+								<ListGroup.Item>
+									Edited Volume: {info.edited_volume}
+								</ListGroup.Item>
+								<ListGroup.Item>
+									Task Received Date: {info.task_received_date}
+								</ListGroup.Item>
+								<ListGroup.Item>
+									Task Created: {info.createdAt.substring(0, 10)}
+									{" Time: "}
+									{info.createdAt.substring(11, 19)}
+								</ListGroup.Item>
+								{info.status === "Done" && (
+									<ListGroup.Item>
+										Task Updated QA DONE: {info.createdAt.substring(0, 10)}
+										{" Time: "}
+										{info.createdAt.substring(11, 19)}
+									</ListGroup.Item>
+								)}
+								{info.status === "Pending" ? (
+									<ListGroup.Item>
+										Status: <span style={{ color: "red" }}>{info.status}</span>
+									</ListGroup.Item>
+								) : (
+									<ListGroup.Item>
+										Status:{" "}
+										<span style={{ color: "green" }}>{info.status}</span>
+									</ListGroup.Item>
+								)}
+							</ListGroup>
+						</Card>
+					))
+				)}
+			</Container>
+		</MainScreen>
+	);
 };
 
 export default TaskList;
